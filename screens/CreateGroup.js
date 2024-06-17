568b54f65312ada77d972c237691cb80818a9ead
Deian <deianandrei22@yahoo.com>
2024-06-17 14:26:19 +0300

    groups have public and visible fields

--- screens/CreateGroup.js
+++ screens/CreateGroup.js
@@ -1,11 +1,11 @@
-import React , { useState , useEffect } from 'react';
-import { View, Text, Button , Image, StyleSheet, TextInput, TouchableOpacity, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, StatusBar } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
 import * as ImagePicker from 'react-native-image-picker';
 
-import defaultPfp from '../img/defaultpfp.png'
+import defaultPfp from '../img/defaultpfp-overlay.png';
 
 function CreateGroup({ navigation }) {
     const [user, setUser] = useState(null);
@@ -15,6 +15,8 @@ function CreateGroup({ navigation }) {
     const [groupName, setGroupName] = useState('');
     const [groupDescription, setDescription] = useState('');
     const [groupId, setGroupId] = useState('');
+    const [groupPublic, setGroupPublic] = useState('');
+    const [groupVisible, setGroupVisible] = useState('');
 
     useEffect(() => {
         const unsubscribe = auth().onAuthStateChanged(user => {
@@ -53,17 +55,12 @@ function CreateGroup({ navigation }) {
                 const selectedImage = response.assets[0];
                 try {
                     const imageRef = storage().ref(`group_profile_images/${groupId}`);
-    
                     await imageRef.putFile(selectedImage.uri);
-    
                     const imageUrl = await imageRef.getDownloadURL();
-    
                     setProfileImage({ uri: imageUrl });
-    
                     await firestore().collection('groups').doc(groupId).update({
                         profileImage: imageUrl,
                     });
-    
                     console.log('Group profile image uploaded successfully.');
                 } catch (error) {
                     console.error('Error uploading group profile image:', error);
@@ -72,7 +69,7 @@ function CreateGroup({ navigation }) {
                 console.log('No image selected.');
             }
         });
-    }
+    };
 
     const handleContinue = async () => {
         try {
@@ -80,13 +77,13 @@ function CreateGroup({ navigation }) {
                 name: groupName,
                 description: groupDescription,
                 profileImage: profileImage.uri, 
-                admin: name, 
+                admin: name,
+                public: groupPublic === 'Yes',
+                visible: groupVisible === 'Yes',
             });
-    
             await firestore().collection('users').doc(user.uid).collection('joinedGroups').doc(groupId).set({
                 groupId: groupId,
             });
-    
             setIsSetUpComplete(true);
             setTimeout(() => {
                 navigation.navigate('Home');
@@ -94,11 +91,11 @@ function CreateGroup({ navigation }) {
         } catch (error) {
             console.error('Error creating group:', error);
         }
-    }
+    };
 
-    const handleCancel = async () => {
-        navigation.navigate('Profile')
-    }
+    const handleCancel = () => {
+        navigation.navigate('Profile');
+    };
 
     return (
         <View style={styles.bigcontainer}>
@@ -106,70 +103,117 @@ function CreateGroup({ navigation }) {
                 <View style={styles.setupContainer}>
                     <Text style={styles.setupText}>The group was created!</Text>
                 </View>
-            )} 
+            )}
             {!isSetUpComplete && (
-            <><Text style={styles.loginText}>Create a group</Text><View style={styles.formContainer}>
-                    <TouchableOpacity onPress={handleImageUpload}>
-                        {profileImage ? (
-                            <Image source={{ uri: profileImage.uri }} style={styles.profileImage} />
-                        ) : (
-                            <Image source={defaultPfp} style={styles.profileImage} />
-                        )}
-                    </TouchableOpacity>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={text => setGroupName(text)}
-                        value={groupName}
-                        placeholder="Group name" />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={text => setDescription(text)}
-                        value={groupDescription}
-                        placeholder="Group description" />
-                    <TouchableOpacity style={styles.loginButton} onPress={handleContinue}>
-                        <Text style={styles.buttonText}>CONTINUE</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.loginButton} onPress={handleCancel}>
-                        <Text style={styles.buttonText}>   CANCEL  </Text>
-                    </TouchableOpacity>
-                </View></>
+                <>
+                    <Text style={styles.loginText}>Create a group</Text>
+                    <View style={styles.formContainer}>
+                        <TouchableOpacity onPress={handleImageUpload}>
+                            {profileImage ? (
+                                <Image source={{ uri: profileImage.uri }} style={styles.profileImage} />
+                            ) : (
+                                <Image source={defaultPfp} style={styles.profileImage} />
+                            )}
+                        </TouchableOpacity>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={text => setGroupName(text)}
+                            value={groupName}
+                            placeholder="Group name"
+                        />
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={text => setDescription(text)}
+                            value={groupDescription}
+                            placeholder="Group description"
+                        />
+                        <Text style={styles.label}>Is public?</Text>
+                        <View style={styles.optionContainer}>
+                            <TouchableOpacity
+                                style={[
+                                    styles.optionButton,
+                                    groupPublic === 'Yes' && styles.selectedOptionButton,
+                                ]}
+                                onPress={() => setGroupPublic('Yes')}
+                            >
+                                <Text style={styles.optionText}>Yes</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={[
+                                    styles.optionButton,
+                                    groupPublic === 'No' && styles.selectedOptionButton,
+                                ]}
+                                onPress={() => setGroupPublic('No')}
+                            >
+                                <Text style={styles.optionText}>No</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.label}>Is visible?</Text>
+                        <View style={styles.optionContainer}>
+                            <TouchableOpacity
+                                style={[
+                                    styles.optionButton,
+                                    groupVisible === 'Yes' && styles.selectedOptionButton,
+                                ]}
+                                onPress={() => setGroupVisible('Yes')}
+                            >
+                                <Text style={styles.optionText}>Yes</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={[
+                                    styles.optionButton,
+                                    groupVisible === 'No' && styles.selectedOptionButton,
+                                ]}
+                                onPress={() => setGroupVisible('No')}
+                            >
+                                <Text style={styles.optionText}>No</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <TouchableOpacity style={styles.loginButton} onPress={handleContinue}>
+                            <Text style={styles.buttonText}>CONTINUE</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.loginButton, styles.cancelButton]} onPress={handleCancel}>
+                            <Text style={styles.buttonText}>CANCEL</Text>
+                        </TouchableOpacity>
+                    </View>
+                </>
             )}
         </View>
     );
 }
-  
-  const styles = StyleSheet.create({
+
+const styles = StyleSheet.create({
     bigcontainer: {
-      flex: 1,
-      backgroundColor: 'black',
-      alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
     },
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'black',
-      paddingTop: StatusBar.currentHeight,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        paddingTop: StatusBar.currentHeight,
     },
     loginText: {
-      fontSize: 36,
-      fontWeight: 'bold',
-      marginTop: 30,
-      color: '#B1EEDB',
-      position: 'relative',
-      textShadowColor: 'rgba(0, 0, 0, 0.75)',
-      textShadowOffset: { width: 2, height: 2 },
-      textShadowRadius: 10,
+        fontSize: 36,
+        fontWeight: 'bold',
+        marginTop: 30,
+        color: '#B1EEDB',
+        position: 'relative',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: { width: 2, height: 2 },
+        textShadowRadius: 10,
     },
     formContainer: {
-      marginTop: 20,
-      width: '80%',
-      padding: 30,
-      borderWidth: 0,
-      borderColor: '#ccc',
-      borderRadius: 10,
-      backgroundColor: '#000000',
-      alignItems: 'center'
+        marginTop: 20,
+        width: '80%',
+        padding: 30,
+        borderWidth: 0,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        backgroundColor: '#000000',
+        alignItems: 'center',
     },
     input: {
         height: 50,
@@ -182,18 +226,21 @@ function CreateGroup({ navigation }) {
         fontSize: 15,
     },
     loginButton: {
-      backgroundColor: '#B1EEDB',
-      padding: 10,
-      alignItems: 'center',
-      borderRadius: 20,
-      borderColor: '#B1EEDB',
-      borderWidth: 2,
-      top: 200,
-      marginTop: 15,
+        backgroundColor: '#B1EEDB',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 20,
+        borderColor: '#B1EEDB',
+        borderWidth: 2,
+        marginTop: 15,
+    },
+    cancelButton: {
+        backgroundColor: 'red',
+        borderColor: 'red',
     },
     buttonText: {
-      color: 'white',
-      fontSize: 15
+        color: 'white',
+        fontSize: 15,
     },
     profileImage: {
         width: 120,
@@ -207,6 +254,28 @@ function CreateGroup({ navigation }) {
         marginBottom: 5,
         fontSize: 20,
     },
+    optionContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 20,
+    },
+    optionButton: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        marginHorizontal: 5,
+    },
+    selectedOptionButton: {
+        backgroundColor: '#B1EEDB',
+        borderColor: '#B1EEDB',
+    },
+    optionText: {
+        color: 'white',
+    },
     setupContainer: {
         alignItems: 'center',
         justifyContent: 'center',
@@ -216,6 +285,6 @@ function CreateGroup({ navigation }) {
         fontSize: 24,
         color: 'white',
     },
-  });
+});
 
 export default CreateGroup;
